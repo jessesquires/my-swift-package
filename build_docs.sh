d457e010edc2916ba2bcb1fd09612511e263f686
Jesse Squires <jesse@jessesquires.com>
2022-04-11 17:46:55 -0700

    remove quotes?

--- build_docs.sh
+++ build_docs.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 swift package --allow-writing-to-directory ./docs \
-generate-documentation --target "my-swift-package" \
+generate-documentation --target my-swift-package \
 --disable-indexing \
 --transform-for-static-hosting \
---hosting-base-path "my-swift-package" \
+--hosting-base-path my-swift-package \
 --output-path ./docs
